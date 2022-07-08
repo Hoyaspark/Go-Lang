521a3b583d8594497550cc4739a481803b881622
박상호 <hoyaspark@gmail.com>
2022-07-08 18:37:16 +0900

    feat : learning func, return

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"awesomeProject/something"
 	"fmt"
+	"strings"
 )
 
 /**
@@ -24,4 +25,36 @@ func main() {
 	insideName := "psh"
 
 	fmt.Println(insideName)
+
+	fmt.Println(multiply(3, 2))
+
+	// _ is ignore varaible
+	totalLength, upperName := lenAndUpper("pedro")
+	length, _ := lenAndUpper("pedro")
+
+	fmt.Println(totalLength, upperName, length)
+
+	repeatMe("nico", "pedro", "marco", "hello")
+
+	fmt.Println(lenAndUpperNaked("pedro"))
+}
+
+func repeatMe(words ...string) {
+	fmt.Println(words)
+}
+
+func lenAndUpperNaked(name string) (length int, upperName string) {
+	defer fmt.Println("I'm done")
+
+	length = len(name)
+	upperName = strings.ToUpper(name)
+	return
+}
+
+func multiply(a, b int) int {
+	return a * b
+}
+
+func lenAndUpper(name string) (int, string) {
+	return len(name), strings.ToUpper(name)
 }
